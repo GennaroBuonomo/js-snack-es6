333e3d1b91a5c0b543be21754890ca29407ce825
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-14 16:09:28 +0200

    feat creo un array oggetti

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+// array di oggetti bici da corsa
+const biciDaCorsa =[
+  { nome: "Bianchi Oltre RC", peso: 6.7},
+  { nome: "Look 795 Blade RS Proteam", peso: 6.8},
+  { nome: "Wilier 0 SLR", peso: 6.9},
+  { nome: "Pinarello Dogma F", peso: 6.2},
+  { nome: "Colnago V4Rs", peso: 6.5},
+]
