9006daf232d2361670d9cfbc89cbe4abddcdedfa
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-14 16:20:20 +0200

    feat creo un array squadre

---
+++ JS/script2.js
@@ -0,0 +1,8 @@
+// array di oggetti squadre di calcio con solo nome, punti e falli a 0
+const squadreCalcio =[
+  { nome: "Milan", punti: 0, falliSubiti: 0 },
+  { nome: "Inter", punti: 0, falliSubiti: 0 },
+  { nome: "Napoli", punti: 0, falliSubiti: 0 },
+  { nome: "Roma", punti: 0, falliSubiti: 0 },
+  { nome: "Juventus", punti: 0, falliSubiti: 0 },
+];
